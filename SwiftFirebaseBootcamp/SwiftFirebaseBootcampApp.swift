53c76825b159dadd4d05b2e959b3e9a6b8f245b3
lmsand <lmsand99@icloud.com>
2024-01-20 04:46:03 -0600

    Initial Commit

---
+++ SwiftFirebaseBootcamp/SwiftFirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFirebaseBootcampApp.swift
+//  SwiftFirebaseBootcamp
+//
+//  Created by lexi sanders on 1/20/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
